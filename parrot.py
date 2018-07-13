eab4790c315540e993067f55de2bc2f1110aa80d
MengleJhon <13260286396@163.com>
2018-07-13 11:16:30 +0800

    Default

---
+++ parrot.py
@@ -0,0 +1,6 @@
+#-*-coding:utf-8-*-
+message = input("Tell me something, and I will repeat it back to you: ")
+print(str(message))
+
+name = input("Please enter your name: ")
+print("Hello, " + str(name) + "!")
