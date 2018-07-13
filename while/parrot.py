3490a8fedab52102d045f67cb57627cd3d969235
MengleJhon <13260286396@163.com>
2018-07-13 15:47:21 +0800

    test project

--- while/parrot.py
+++ while/parrot.py
@@ -1,6 +1,29 @@
 prompt = "\nTell me something, and I will repeat it back to you: "
-prompt += "\nEnter 'quit' to end the program."
-message = ""
-while message != 'quit':
+prompt += "\n(Enter 'quit' to end the program.)"
+
+# 第一版
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#
+#     if message != 'quit':
+#         print(message)
+
+# 第二版：使用flag
+# active = True
+# while active:
+#     message = input(prompt)
+#
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+
+# 使用break
+while True:
     message = input(prompt)
-    print(message)
+
+    if message == 'quit':
+        break
+    else:
+        print(message)
