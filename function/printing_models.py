35f4bea135454c9ae5d0410b8375170c913ad0e9
MengleJhon <13260286396@163.com>
2018-07-17 15:38:50 +0800

    test project

--- function/printing_models.py
+++ function/printing_models.py
@@ -41,14 +41,3 @@ show_completed_models(completed_models)
 # print_models(unprinted_designs[:],completed_models)
 # show_completed_models(completed_models)
 # show_completed_models(unprinted_designs)
-
-
-def make_pizza(size,*toppings):
-    """概述要制作的比萨"""
-    print("\nMaking a " + str(size) +
-          "-inch pizza with the fpllowing toppings:")
-    for topping in toppings:
-        print("- " + topping)
-
-make_pizza(16,'pepperoni')
-make_pizza(12,'mushrooms','green peppers','extra cheese')
