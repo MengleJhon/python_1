c23d9495c1225b7e349684359535efbfaaec8e63
MengleJhon <13260286396@163.com>
2018-07-17 10:11:18 +0800

    test project

--- function/printing_models.py
+++ function/printing_models.py
@@ -37,3 +37,7 @@ completed_models = []
 
 print_models(unprinted_designs,completed_models)
 show_completed_models(completed_models)
+
+# print_models(unprinted_designs[:],completed_models)
+# show_completed_models(completed_models)
+# show_completed_models(unprinted_designs)
