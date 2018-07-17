154af5b490b7775ad71a390a023ac6b01518b4a6
MengleJhon <13260286396@163.com>
2018-07-17 21:41:20 +0800

    test project

--- class/car.py
+++ class/car.py
@@ -56,11 +56,12 @@ class ElectricCar(Car):
     def __init__(self,make,model,year):
         """初始化父类的属性"""
         super().__init__(make,model,year)
-        self.battery_size = 70
+        # self.battery_size = 70
+        self.battery = Battery()
 
     def describe_battery(self):
         """打印一条描述电瓶容量的"""
-        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+        print("This car has a " + str(self.battery.battery_size) + "-kWh battery.")
 
     def get_descriptive_name(self):
         """重写父类方法"""
@@ -71,7 +72,36 @@ class ElectricCar(Car):
         """电动汽车没有油箱"""
         print("This car doesn't need a gas tank!")
 
+# my_tesla = ElectricCar('tesla','model s',2016)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.describe_battery()
+# my_tesla.fill_gas_tank()
+
+class Battery():
+    """一次模拟电动汽车电瓶的简单尝试"""
+
+    def __init__(self,battery_size=70):
+        """初始化电瓶的属性"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """打印一条描述电瓶容量的消息"""
+        print("This car has a " + str(self.battery_size) + "kWh battery.")
+
+    def get_range(self):
+        """打印一条消息，指出电瓶的续航里程"""
+        if self.battery_size == 70:
+            range = 240
+        else:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
 my_tesla = ElectricCar('tesla','model s',2016)
+
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
-my_tesla.fill_gas_tank()
+my_tesla.battery.describe_battery()
+
+my_tesla.battery.get_range()
