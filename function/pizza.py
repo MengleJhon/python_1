a2509c8de98abdf80e9a87e2d7ce02b18f150fc7
MengleJhon <13260286396@163.com>
2018-07-17 15:38:22 +0800

    test project

---
+++ function/pizza.py
@@ -0,0 +1,10 @@
+#-*-coding:utf-8-*-
+def make_pizza(size,*toppings):
+    """概述要制作的比萨"""
+    print("\nMaking a " + str(size) +
+          "-inch pizza with the fpllowing toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+# make_pizza(16,'pepperoni')
+# make_pizza(12,'mushrooms','green peppers','extra cheese')
