0f391f306e650b49eaa1fe7cfa0514ec639c93ce
MengleJhon <13260286396@163.com>
2018-08-08 16:56:15 +0800

    test project

---
+++ python1.0/python_study1/test.py
@@ -0,0 +1,10 @@
+print('{a} a word in {b}.'.format(a = 'with',b = 'came'))
+print('{0} a word in {1}.'.format('with','came'))
+print('{} a word in {}.'.format('with','came') + str(9//2))
+
+a = [15,True]
+print(a[-1])
+
+print(5,'word')
+print('hello' in a)
+
